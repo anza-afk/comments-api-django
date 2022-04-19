3042411990fae202023e82ce6cc537c9931a5112
Anatoly Zapravadov <zapokami@gmail.com>
2022-04-19 12:44:56 +0300

    fix

--- app/api/serializers.py
+++ app/api/serializers.py
@@ -8,24 +8,32 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         model = User
         fields = ('url', 'username', 'articles', 'comments')
 
-class NestedCommentSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Comment
-        fields = ['child_comment']
 
 class CommentSerializer(serializers.HyperlinkedModelSerializer):
-    child_comment = NestedCommentSerializer(many=True)
+    user = serializers.ReadOnlyField(source='user.url')
+    article = serializers.ReadOnlyField(source='article.url')
     class Meta:
         model = Comment
         fields = ('url', 'content', 'created_date', 'article', 'user', 'parent', 'child_comment')
+    def to_representation(self, instance):
+        self.fields['child_comment'] = CommentSerializer(read_only=True, many=True)
+        return super(CommentSerializer, self).to_representation(instance)
+
+
+class ArticleCommentSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ('url', 'user', 'content', 'created_date', 'child_comment')
+    def to_representation(self, instance):
+        self.fields['child_comment'] = ArticleCommentSerializer(read_only=True, many=True)
+        return super(ArticleCommentSerializer, self).to_representation(instance)
 
 
 class ArticleSerializer(serializers.HyperlinkedModelSerializer):
-    comments = CommentSerializer(many=True, read_only=True)
+    comments = ArticleCommentSerializer(many=True, read_only=True)
 
     class Meta:
         model = Article
         fields = ('url', 'name', 'title', 'content', 'created_date', 'user', 'comments')
-
 
 
